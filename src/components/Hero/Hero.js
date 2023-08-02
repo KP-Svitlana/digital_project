4a0b14d7220743eab9ffd8ec2dabd3af1de24cf0
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-08-02 20:42:39 +0300

    update: hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,28 +7,34 @@ import heroImg from "@/accets/img/hero_1x.png";
 
 export const Hero = () => {
   return (
-    <section className="container">
-      <h1 className="title">
-        Project <br /> <span className="title__span">Home</span>
-      </h1>
-      <div className="hero__btnWrap">
-        <button type="button" className="hero__btn">
-          <Image src={leftErrowSvg} alt="Left errow" />
-        </button>
-        <button type="button" className="hero__btn">
-          <Image src={rightErrowSvg} alt="Right errow" />
-        </button>
+    <section className="container hero__container ">
+      <div>
+        <h1 className="title">
+          Project <br /> <span className="title__span">Home</span>
+        </h1>
+        <div className="hero__btnWrap">
+          <button type="button" className="hero__btn">
+            <Image src={leftErrowSvg} alt="Left errow" />
+          </button>
+          <button type="button" className="hero__btn">
+            <Image src={rightErrowSvg} alt="Right errow" />
+          </button>
+        </div>
+        <div className="hero__numBtnWrap">
+          <button type="button" className="hero__numBtn">
+            01
+          </button>
+          <button type="button" className="hero__numBtn">
+            02
+          </button>
+        </div>
       </div>
-      <div className="hero__numBtnWrap">
-        <button type="button" className="hero__numBtn">
-          01
-        </button>
-        <button type="button" className="hero__numBtn">
-          02
-        </button>
+      <div className="hero__imgWrap">
+        <Image src={heroImg} alt="Photo of building" className="hero__img" />
+        <div className="btnWpap">
+          <Button text={"Взглянуть"} isDark={false} />
+        </div>
       </div>
-      <Image src={heroImg} alt="Photo of building" />
-      <Button text={"Взглянуть"} isDark={false} />
     </section>
   );
 };
