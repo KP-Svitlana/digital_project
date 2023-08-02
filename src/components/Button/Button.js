35981dc4549b182b48b8bfe0a197d2100e62fcf0
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-08-02 19:09:50 +0300

    update: hero section

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import errowSvg from "@/accets/svg/errow.svg";
+import errowSvgDark from "@/accets/svg/rightErrowBlack.svg";
 
-export const Button = ({ text }) => {
+export const Button = ({ text, isDark }) => {
   return (
-    <Link href="#" className="button">
+    <Link href="#" className={isDark ? "button" : "button--white"}>
       {text}
-      <Image src={errowSvg} alt="Errow" />
+      <Image src={isDark ? errowSvg : errowSvgDark} alt="Errow" />
     </Link>
   );
 };
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import { Button } from "@/components/Button";
+
+import rightErrowSvg from "@/accets/svg/rightErrowBlack.svg";
+import leftErrowSvg from "@/accets/svg/leftErrow.svg";
+import heroImg from "@/accets/img/hero_1x.png";
+
 export const Hero = () => {
   return (
-    <section>
-      <h1>Project Home</h1>
+    <section className="container">
+      <h1 className="title">
+        Project <br /> <span className="title__span">Home</span>
+      </h1>
+      <div className="hero__btnWrap">
+        <button type="button" className="hero__btn">
+          <Image src={leftErrowSvg} alt="Left errow" />
+        </button>
+        <button type="button" className="hero__btn">
+          <Image src={rightErrowSvg} alt="Right errow" />
+        </button>
+      </div>
+      <div className="hero__numBtnWrap">
+        <button type="button" className="hero__numBtn">
+          01
+        </button>
+        <button type="button" className="hero__numBtn">
+          02
+        </button>
+      </div>
+      <Image src={heroImg} alt="Photo of building" />
+      <Button text={"Взглянуть"} isDark={false} />
     </section>
   );
 };
