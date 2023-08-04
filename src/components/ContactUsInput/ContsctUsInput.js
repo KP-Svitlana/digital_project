7a43506a27edc5e0eb72e0c2daea54481fd89ad6
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-08-04 22:56:31 +0300

    updare: contactUs input logic

--- src/components/ContactUsInput/ContsctUsInput.js
+++ src/components/ContactUsInput/ContsctUsInput.js
@@ -5,43 +5,28 @@ import { useState } from "react";
 export const ContactUsInput = ({ name, type, placeholder, required }) => {
   const [isActive, setIsActive] = useState(false);
 
-  const handleFocus = () => {
+  const handleChange = () => {
     setIsActive(true);
-  };
-
-  const handleBlur = () => {
-    setIsActive(false);
   };
 
   return (
     <>
-      {type ? (
-        <div className="input_Wrap">
+      <div className="input_Wrap">
+        {type ? (
           <input
             className="form__input"
             name={name}
             type={type}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
+            onChange={handleChange}
           />
-
-          <div
-            className={
-              isActive ? "input__placeholder--hidden" : "input__placeholder"
-            }
-          >
-            {placeholder}
-            {required && <span className="input__star">*</span>}
-          </div>
-        </div>
-      ) : (
-        <div className="input_Wrap">
+        ) : (
           <textarea
             className="form__textarea"
             name={name}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
+            onChange={handleChange}
           ></textarea>
+        )}
+        {!isActive && (
           <div
             className={
               isActive ? "input__placeholder--hidden" : "input__placeholder"
@@ -50,8 +35,45 @@ export const ContactUsInput = ({ name, type, placeholder, required }) => {
             {placeholder}
             {required && <span className="input__star">*</span>}
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
+    //   // {/* {type ? (
+    //     <div className="input_Wrap">
+    //       <input
+    //         className="form__input"
+    //         name={name}
+    //         type={type}
+    //         onFocus={handleFocus}
+    //         onBlur={handleBlur}
+    //       />
+    //       <div
+    //         className={
+    //           isActive ? "input__placeholder--hidden" : "input__placeholder"
+    //         }
+    //       >
+    //         {placeholder}
+    //         {required && <span className="input__star">*</span>}
+    //       </div>
+    //     </div>
+    //   ) : (
+    //     <div className="input_Wrap">
+    //       <textarea
+    //         className="form__textarea"
+    //         name={name}
+    //         onFocus={handleFocus}
+    //         onBlur={handleBlur}
+    //       ></textarea>
+    //       <div
+    //         className={
+    //           isActive ? "input__placeholder--hidden" : "input__placeholder"
+    //         }
+    //       >
+    //         {placeholder}
+    //         {required && <span className="input__star">*</span>}
+    //       </div>
+    //     </div>
+    //   )} */}
+    // </>
   );
 };
