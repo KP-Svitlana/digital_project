05d2b65cd4e19fe2be5391b3b07bd319f8b561f2
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-08-04 12:55:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/github.com/KP-Svitlana/digital_project",
+  basePath: "/KP-Svitlana/digital_project",
 };
 
 module.exports = nextConfig;
