8c6627cdeb643ebea38082fb152f7744dfd93f1a
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-08-04 15:14:45 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: "/digital_project/",
   basePath: "/digital_project",
 };
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -26,7 +26,7 @@ export const About = () => {
           <h2 className="section__title">О компании</h2>
           <p className="about__text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
